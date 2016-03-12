6c85a7f7322e47afdb4840184a616e7ba3ab0652
Marvin Liu <marvinliu9@gmail.com>
2016-03-13 00:20:47 +0800

    finish first model and added a writeup

--- model.py
+++ model.py
@@ -7,24 +7,29 @@ from sklearn.ensemble import GradientBoostingClassifier
 
 # load the data
 print('Loading data...')
-projects = pd.read_csv('projects.csv')
-outcomes = pd.read_csv('outcomes.csv')
-print('Data loaded...')
+outcomes_df = pd.read_csv('outcomes.csv')
+projects_df = pd.read_csv('projects.csv')
+# donations_df = pd.read_csv('donations.csv')
 
-# sort the data based on id
-projects = projects.sort_values(by='projectid')
-outcomes = outcomes.sort_values(by='projectid')
+print('Filling missing data...')
+# fill the missing data with previous observation data
+projects_df = projects_df.fillna(method='pad')
 
-print ('Projects shape', projects.shape)
-print ('Outcomes shape', outcomes.shape)
 
+print('Splitting data...')
 # split the training data and testing data
-dates = np.array(projects.date_posted)
+
+dates = np.array(projects_df.date_posted)
+out_idx = np.where(dates < '2010-04-01')[0]
 train_idx = np.where(dates < '2014-01-01')[0]
+
+# disregard data before April 2010
+# train_idx = np.setdiff1d(train_idx, out_idx)
+
 test_idx = np.where(dates >= '2014-01-01')[0]
 
-# fill the missing data with previous observation data
-projects = projects.fillna(method='pad')
+# df is a left outer join of projects and outcomes on projectid
+df = pd.merge(projects_df, outcomes_df, how='left', on='projectid')
 
 #preprocessing the data based on different types of attributes
 projects_numeric_columns = ['school_latitude', 'school_longitude',
@@ -34,11 +39,11 @@ projects_numeric_columns = ['school_latitude', 'school_longitude',
 
 projects_id_columns = ['projectid', 'teacher_acctid', 'schoolid', 'school_ncesid']
 
-projects_categorial_columns = np.array(list(set(projects.columns).difference(set(projects_numeric_columns)).difference(set(projects_id_columns)).difference(set(['date_posted']))))
-projects_categorial_values = np.array(projects[projects_categorial_columns])
+projects_categorial_columns = np.array(list(set(projects_df.columns).difference(set(projects_numeric_columns)).difference(set(projects_id_columns)).difference(set(['date_posted']))))
+
+projects_categorial_values = np.array(df[projects_categorial_columns])
 
 print('Label encoding...')
-
 # encode each categorical column and store in projects_data
 label_encoder = LabelEncoder()
 projects_data = label_encoder.fit_transform(projects_categorial_values[:,0])
@@ -52,11 +57,11 @@ print('projects_data shape after label encoding', projects_data.shape)
 
 #Predicting
 xTr = projects_data[train_idx]
-yTr = np.array(outcomes.is_exciting)
+yTr = np.array(df.is_exciting[train_idx])
 xTe = projects_data[test_idx]
 
 print('Splitting training set into training and validation sets...')
-xTrain, xVal, yTrain, yVal = cross_validation.train_test_split(xTr, yTr, test_size=0.2, random_state=42)
+xTrain, xVal, yTrain, yVal = cross_validation.train_test_split(xTr, yTr, test_size=0.3, random_state=42)
 
 print('Training gradient boosting classifier...')
 clf = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0, max_depth=3, random_state=0)
