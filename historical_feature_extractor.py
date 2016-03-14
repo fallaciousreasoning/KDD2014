370bef160875bc229076b203552ba1e5f6dddb87
Jay Harris <jay.harris@outlook.co.nz>
2016-03-14 22:16:06 +0800

    Edits features.txt and begins work on extracting historical features

--- historical_feature_extractor.py
+++ historical_feature_extractor.py
@@ -19,7 +19,31 @@ print('Loading data...')
 projects_df = pd.read_csv('projects_small.csv')
 donations_df = pd.read_csv('donations_small.csv')
 
+project_information = dict()
+
+teacher_totals = dict()
+teacher_donation_counts = dict()
+teacher_exciting_count = dict()
+
+area_totals = dict()
+area_exciting_count = dict()
+area_donation_counts = dict()
+
+for row in projects_df.itertuples()
+
+for row in donations_df.itertuples():
+	if row.is_teacher_acct == 'f':
+		continue
+	
+	id = row.donor_acctid
+	#if we haven't seen the id, put it in the dictionary
+	if not id in teachers:
+		teachers.update({id: 0})
+
+	teachers[id] = teachers[id] + row.donation_total
+
+print(teachers)
 #Calculate information about teachers and projects
 teachers_and_projects = projects_df[['projectid', 'teacher_acctid']]
 
-print(teachers_and_projects.groupby('teacher_acctid'))
+#print(teachers_and_projects.groupby('teacher_acctid'))
