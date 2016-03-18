c7968a4d97813c91b824d69d81b781c2638ecd7d
Jay Harris <jay.harris@outlook.co.nz>
2016-03-18 13:34:01 +0800

    Improves commenting on the CFE

--- categorical_feature_extractor.py
+++ categorical_feature_extractor.py
@@ -1,3 +1,4 @@
+#import the things we need
 import csv
 import pandas as pd
 import numpy as np
@@ -22,7 +23,10 @@ projects_numeric_columns = ['school_latitude',
 
 projects_id_columns = ['teacher_acctid', 'schoolid', 'school_ncesid']
 
+#load the projects cv and remove all the numeric columns
 projects_categorial_columns = np.array(list(set(projects_df.columns).difference(set(projects_numeric_columns)).difference(set(projects_id_columns))))
+
+#take all the values from projects that can be categorical features
 projects_categorial_values = projects_df[projects_categorial_columns]
 
 print 'Binarizing categorical values...'
@@ -30,8 +34,11 @@ print 'Binarizing categorical values...'
 for key in projects_categorial_columns:
   if key in ['projectid', 'date_posted', 'students_reached']: 
     continue
+
+  #set the value for text base categories to their number of occurrences throughout the dataset
   projects_categorial_values[key] = projects_df.groupby(key)[key].transform('count')
   print 'Binarized %s' % key
 
 print 'Writing categorical_features.csv...'
+#save the data to a categorical data csv
 projects_categorial_values.to_csv('categorical_features.csv')
